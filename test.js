4d80764d3f620e674273fe3753f4b619fc4d535d
Dylan Awalt-Conley <web@dylan.ac>
2021-04-23 14:40:42 -0400

    switched from my secret manager to keytar, to work accross dev platforms

--- test.js
+++ test.js
@@ -1,11 +1,20 @@
-const Secret = require('@dawaltconley/secret')
+const { prompt, whisper } = require('@dawaltconley/cue');
+const keytar = require('keytar')
 const TrackMOC = require('./index.js')
 
-const proPublica = new Secret('https://api.propublica.org/congress/v1')
+const getKey = async (service='api.propublica.org') => {
+    let key = await keytar.findPassword(service);
+    if (!key) {
+        console.log(`Couldn't find key for ${service}. Please enter manually.`);
+        let account = await prompt('Account:')
+        key = await whisper('API Key:')
+        await keytar.setPassword(service, account, key);
+    }
+    return key;
+}
 
-proPublica.get()
-    .then(async ({ password }) => {
-        const tracker = new TrackMOC({ key: password })
-        const members = await tracker.updateMems()
-        console.log(members)
-    })
+getKey().then(async key => {
+    const tracker = new TrackMOC({ key: key })
+    const members = await tracker.updateMems()
+    console.log(members)
+})
