97f51c901a50683c76781dd2c70a5a5fef2145ed
Dylan Awalt-Conley <web@dylan.ac>
2021-04-26 01:12:47 -0400

    expaneded tests, saving files locally for reference

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 const { prompt, whisper } = require('@dawaltconley/cue');
 const keytar = require('keytar');
 const TrackMOC = require('./index.js');
+const fsp = require('fs').promises;
+
+const fileExists = path => fsp.access(path)
+    .then(() => true)
+    .catch(e => e.code === 'ENOENT' ? false : e);
 
 const getKey = async (service='api.propublica.org') => {
     let key = await keytar.findPassword(service);
@@ -13,8 +18,23 @@ const getKey = async (service='api.propublica.org') => {
     return key;
 };
 
-getKey().then(async key => {
-    const tracker = new TrackMOC({ key: key });
-    const members = await tracker.updateMems();
-    console.log(members);
-});
+getKey().then(k => new TrackMOC({ key: k }))
+    .then(async tracker => {
+        if (await fileExists('./examples/members.json')) {
+            console.log('local reps exist');
+            let reps = await fsp.readFile('./examples/members.json');
+            console.log('loaded reps');
+            tracker.reps = JSON.parse(reps);
+        }
+        let members = await tracker.updateMems();
+        let bill = await tracker.getBill('hconres83-116');
+        let vote = await tracker.getVote('hconres83-116');
+        console.dir({ bill }, { depth: null });
+        let local = [];
+        local.push(fsp.writeFile('./examples/members.json', JSON.stringify(members)));
+        local.push(fsp.writeFile('./examples/bill.json', JSON.stringify(bill)));
+        local.push(fsp.writeFile('./examples/vote.json', JSON.stringify(vote)));
+        return Promise.all(local);
+    })
+    .then(() => console.log('done'))
+    .catch(e => console.error(e));
