68810ce4e7b7d381a83329b738e7d0fe08ec1dcb
Dylan Awalt-Conley <web@dylan.ac>
2021-06-25 20:08:21 -0400

    updated test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const cmd = require('./commands/record.js').handler;
+const cmd = require('./commands/records.js').handler;
 
 cmd({
     cosponsors: [ 'sjres10', 'hr256', 'hr3261', 'hr2014', 'hjres114-107', 'sjres46-107' ],
-    votes: [ 'h.1.464-116', 'S.1.195-115', 'hr256', 'hjres114-107' ],
+    votes: [ 'hr550-116', 'h.1.464-116', 'S.1.195-115', 'hr256', 'hjres114-107' ],
     file: './examples/aumf.csv'
 });
